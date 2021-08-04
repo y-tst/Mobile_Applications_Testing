bd0e7bb449057e57a3fbcbb6154c07f572566f94
y-tst <yurii_yanchuk@epam.com>
2021-08-04 09:01:07 +0300

    Added assert and refactoring

--- src/test/java/scenarios/ClickAddContactButtonTest.java
+++ src/test/java/scenarios/ClickAddContactButtonTest.java
@@ -1,11 +1,17 @@
 package scenarios;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import service.DriverSetup;
+
+import java.util.logging.Logger;
 
 public class ClickAddContactButtonTest extends DriverSetup {
+
+    public Logger logger = Logger.getGlobal();
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -13,12 +19,18 @@ public class ClickAddContactButtonTest extends DriverSetup {
     }
 
     @Test
-    public void clickButtonTest() {
+    public void clickButtonTest() throws InterruptedException {
         String appPackageName = "com.example.android.contactmanager:id/";
         By addButton = By.id(appPackageName + "addContactButton");
         driver.findElement(addButton).click();
 
-        System.out.println("The Add Contact button is clicked");
+        Thread.sleep(3000);
+
+        String pageTitle = driver.findElement(By.id("android:id/title")).getText();
+
+        logger.info(String.format("The Add Contact button is clicked, page title is: %s", pageTitle));
+
+        Assert.assertEquals("Incorrect page Opened","Add Contact", pageTitle);
     }
 
     @AfterClass
