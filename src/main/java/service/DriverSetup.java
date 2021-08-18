08e0f1b02737b4d27b793864eb4ee483cf01f3c2
y-tst <yurii_yanchuk@epam.com>
2021-08-18 17:38:57 +0300

    Hardcode removing

--- src/main/java/service/DriverSetup.java
+++ src/main/java/service/DriverSetup.java
@@ -9,17 +9,22 @@ import java.net.URL;
 
 public class DriverSetup {
     protected static AndroidDriver driver;
+    private static final String TEST_DEVICE = "emulator-5554";
+    private static final String TEST_PLATFORM = "Android";
+    private static final String APPLICATION_PATH = "src\\main\\resources";
+    private static final String APPLICATION_NAME = "ContactManager.apk";
+    private static final String HOST_URL = "http://127.0.0.1:4723/wd/hub";
 
-    protected static void  prepareAndroidNative() throws MalformedURLException{
+    protected static void prepareAndroidNative() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName", "emulator-5554");
-        capabilities.setCapability("plaformName","Android");
+        capabilities.setCapability("deviceName", TEST_DEVICE);
+        capabilities.setCapability("platformName", TEST_PLATFORM);
 
-        File appDir = new File("D:\\Learning\\Automation_Mentoring\\M13_mobile_testing\\sample-code-master\\sample-code\\apps\\ContactManager");
-        File app = new File(appDir, "ContactManager.apk");
+        File appDir = new File(APPLICATION_PATH);
+        File app = new File(appDir, APPLICATION_NAME);
 
         capabilities.setCapability("app", app.getAbsolutePath());
 
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL(HOST_URL), capabilities);
     }
 }
